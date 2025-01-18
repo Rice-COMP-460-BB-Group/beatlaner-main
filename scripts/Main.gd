9a9a4b5db24dce36b70c370fcea82c207b6a2357
sitkaspruce054 <fernandogcz1154@gmail.com>
2025-01-18 11:28:32 -0600

    map moves i think

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+var map_scene = preload("res://map/Map.tscn")
+var camera_scene = preload("res://camera/mainCamera.tscn")
+
 func _ready():
-	print("Game started!")
+	print("game started")
+	var map = map_scene.instantiate()
+	var cam = camera_scene.instantiate()
+	add_child(map)
+	add_child(cam)
+
+	
