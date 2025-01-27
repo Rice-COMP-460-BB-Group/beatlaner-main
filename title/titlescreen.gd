23d8ff99778f8752ba23a2b0b6cfcd2edf7bcbc6
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-01-26 19:39:23 -0600

    Add button sounds

--- title/titlescreen.gd
+++ title/titlescreen.gd
@@ -4,8 +4,16 @@ func _ready():
 	$VBoxContainer/Start.grab_focus()
 
 func _on_start_pressed() -> void:
+	$Confirm.play()
+	await $Confirm.finished
 	get_tree().change_scene_to_file("res://main/Main.tscn")
 
 
 func _on_exit_pressed() -> void:
+	$Confirm.play()
+	await $Confirm.finished
 	get_tree().quit()
+
+
+func _on_button_focus_entered() -> void:
+	$Focus.play()
