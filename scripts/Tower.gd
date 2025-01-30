a477cc966b56242151c4ebb9db339b2d7c83ef78
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-01-30 15:29:14 -0600

    Make sure not to attack own minions

--- scripts/Tower.gd
+++ scripts/Tower.gd
@@ -38,7 +38,7 @@ func _physics_process(delta: float) -> void:
 	if last_attack < 0 or last_attack > attack_speed:
 		var enemy_minions = []
 		for body in $DetectionArea.get_overlapping_bodies():
-			if body is Minion:
+			if body is Minion and body.team != team:
 				var space_state = get_world_2d().direct_space_state
 				var query = PhysicsRayQueryParameters2D.create(global_position, body.global_position, (1 | 3), [self])
 
