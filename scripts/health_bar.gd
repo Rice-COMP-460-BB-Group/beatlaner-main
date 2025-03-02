b9fed6043e3ec9ded8ec108f787648a22be68e50
OrigamiPaper225 <jl322@rice.edu>
2025-03-01 18:16:58 -0600

    health component not updating after upgrade

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -6,3 +6,6 @@ func _ready():
 func update(new_health, max_health):
 	var percent = float(new_health) / float(max_health) * 100.0
 	value = percent
+
+func incresae_Health(new_health):
+	value = 100
