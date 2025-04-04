dea78933455030761ef4279d44456810bdb6cb46
OrigamiPaper225 <jl322@rice.edu>
2025-04-04 15:11:12 -0500

    results WIP

--- map/game_over.gd
+++ map/game_over.gd
@@ -19,34 +19,54 @@ func update_stats_display():
 		var stats_text = "Player %d:\n" % idx  # Header for each player
 		
 		idx += 1
-		# Iterate over each stat for the player
-		for stat_name in all_stats[player_id]:
-			# Special handling for match_length
-			if stat_name == "match_length":
-				if player_id == multiplayer.get_unique_id():
-					# Get the MatchLength label and update it directly
-					var match_length_label = get_node("VBoxContainer/MatchLength")
-					if match_length_label != null:
-						var total_seconds = all_stats[player_id][stat_name] / 1000  # Convert ms to seconds
-						var hours = total_seconds / 3600
-						var minutes = (total_seconds % 3600) / 60
-						var seconds = total_seconds % 60
-						match_length_label.text = "%02d:%02d:%02d" % [hours, minutes, seconds]
-						match_length_label.text = str(all_stats[player_id][stat_name])
-					else:
-						print("MatchLength label not found!")
-				continue  # Skip this stat and move to the next one
-			if stat_name == "total_damage_dealt":
-				stats_text +=  "- %s: %s\n" % str(all_stats[player_id][stat_name]).pad_decimals(2)
-			# Add other stats to stats_text
-			else:
-				stats_text += "- %s: %s\n" % [stat_name, all_stats[player_id][stat_name]]
+		var titles = []
 		
-		# Append to the StatsLabel
-		stats_label.text += stats_text + "\n"  # Extra line for spacing
-
-
-
+		if all_stats[player_id]["minion_kill_count"] > 100:
+			titles.append("The Minion Slayer")
+		if all_stats[player_id]["osu_highest_combo"] > 100:
+			titles.append("Probably knew Epstein")
+		if all_stats[player_id]["total_damage_dealt"] > 10000:
+			titles.append("You play too much League")
+		if all_stats[player_id]["death_count"] > 10:
+			titles.append("You suck")
+		if all_stats[player_id]["death_count"] == 0:
+			titles.append("Shinigami")
+		print(titles)
+		if titles.size() > 0:
+			stats_label.text += "\n".join(titles)
+		var match_length_label = get_node("VBoxContainer/MatchLength")
+		if match_length_label != null:
+			var total_seconds = all_stats[player_id]['match_length'] / 1000  # Convert ms to seconds
+			var hours = total_seconds / 3600
+			var minutes = (total_seconds % 3600) / 60
+			var seconds = total_seconds % 60
+			match_length_label.text = "%02d:%02d:%02d" % [hours, minutes, seconds]
+			#match_length_label.text = str(all_stats[player_id]['match_length'])
+		# Iterate over each stat for the player
+		#for stat_name in all_stats[player_id]:
+			## Special handling for match_length
+			#if stat_name == "match_length":
+				#if player_id == multiplayer.get_unique_id():
+					## Get the MatchLength label and update it directly
+					#var match_length_label = get_node("VBoxContainer/MatchLength")
+					#if match_length_label != null:
+						#var total_seconds = all_stats[player_id][stat_name] / 1000  # Convert ms to seconds
+						#var hours = total_seconds / 3600
+						#var minutes = (total_seconds % 3600) / 60
+						#var seconds = total_seconds % 60
+						#match_length_label.text = "%02d:%02d:%02d" % [hours, minutes, seconds]
+						#match_length_label.text = str(all_stats[player_id][stat_name])
+					#else:
+						#print("MatchLength label not found!")
+				#continue  # Skip this stat and move to the next one
+			#if stat_name == "total_damage_dealt":
+				#stats_text += "- %s: %.2f\n" % [stat_name, all_stats[player_id][stat_name]]
+			## Add other stats to stats_text
+			#else:
+				#stats_text += "- %s: %s\n" % [stat_name, all_stats[player_id][stat_name]]
+			#print('stat name', stat_name, all_stats[player_id][stat_name])
+		## Append to the StatsLabel
+		#stats_label.text += stats_text + "\n"  # Extra line for spacing
 
 func _ready() -> void:
 	$"VBoxContainer/Main Menu".grab_focus()
@@ -68,3 +88,7 @@ func _on_main_menu_pressed() -> void:
 	self.hide()
 	get_tree().change_scene_to_file.bind("res://title/titlescreen.tscn").call_deferred()
 	#MatchStats.reset_stats()
+	
+func _on_results_pressed() -> void:
+	self.hide()
+	get_tree().change_scene_to_file.bind("res://results/resultscreen.tscn").call_deferred()
